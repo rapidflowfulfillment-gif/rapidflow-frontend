87984a1f077c5e4fd9aa1361f4ea0710aa41d45a
Raju Ahmmed <ahraju2003@gmail.com>
2025-07-24 04:05:19 +0600

    add backend hosting url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Register from "@/components/register";
 // import Testimonials from "@/components/testimonials-section";
 // import CtaSection from "@/components/cta-section";
 
+export const runtime = "edge";
+
 const HomePage = () => {
   return (
     <div>
